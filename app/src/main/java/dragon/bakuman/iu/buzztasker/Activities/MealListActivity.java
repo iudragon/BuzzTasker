bfe875c647f101d23ed541b656aa921c7df47086
iudragon <42891947+TeamLewok@users.noreply.github.com>
2019-03-09 23:50:55 +0530

    32REALGettingMenuData

--- app/src/main/java/dragon/bakuman/iu/buzztasker/Activities/MealListActivity.java
+++ app/src/main/java/dragon/bakuman/iu/buzztasker/Activities/MealListActivity.java
@@ -3,6 +3,7 @@ package dragon.bakuman.iu.buzztasker.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,22 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import dragon.bakuman.iu.buzztasker.Objects.Restaurant;
 import dragon.bakuman.iu.buzztasker.R;
 
 public class MealListActivity extends AppCompatActivity {
@@ -32,7 +49,7 @@ public class MealListActivity extends AppCompatActivity {
                 return 3;
             }
 
-             @Override
+            @Override
             public Object getItem(int position) {
                 return null;
             }
@@ -44,7 +61,7 @@ public class MealListActivity extends AppCompatActivity {
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
-                return LayoutInflater.from(MealListActivity.this).inflate(R.layout.list_item_meal, null );
+                return LayoutInflater.from(MealListActivity.this).inflate(R.layout.list_item_meal, null);
             }
         });
 
@@ -55,5 +72,34 @@ public class MealListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        getMeals(restaurantId);
+    }
+
+    private void getMeals(String restaurantId) {
+
+        String url = getString(R.string.API_URL) + "/customer/meals/" + restaurantId + "/";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Log.d("MEAL LIST", response.toString());
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjectRequest);
     }
 }
