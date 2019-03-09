1f9efa473c87e2eb467160a2fd06587b8b22d8a3
iudragon <42891947+TeamLewok@users.noreply.github.com>
2019-03-09 18:15:03 +0530

    20GettingFacebookData

--- app/src/main/java/dragon/bakuman/iu/buzztasker/SignInActivity.java
+++ app/src/main/java/dragon/bakuman/iu/buzztasker/SignInActivity.java
@@ -1,6 +1,8 @@
 package dragon.bakuman.iu.buzztasker;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,11 +10,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -22,6 +30,8 @@ public class SignInActivity extends AppCompatActivity {
 
     Button customerButton, driverButton;
     CallbackManager callbackManager;
+
+    SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +87,7 @@ public class SignInActivity extends AppCompatActivity {
 
 
         callbackManager = CallbackManager.Factory.create();
+        sharedPref = getSharedPreferences("MY_KEY", Context.MODE_PRIVATE);
 
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
@@ -84,6 +95,35 @@ public class SignInActivity extends AppCompatActivity {
                     public void onSuccess(LoginResult loginResult) {
                         // App code
                         Log.d("FACEBOOK TOKEN", loginResult.getAccessToken().getToken());
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(),
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(
+                                            JSONObject object,
+                                            GraphResponse response) {
+                                        // Application code
+                                        Log.d("FACEBOOK DETAILS", object.toString());
+
+
+                                        SharedPreferences.Editor editor = sharedPref.edit();
+
+                                        try {
+                                            editor.putString("name", object.getString("name"));
+                                            editor.putString("email", object.getString("email"));
+                                            editor.putString("avatar", object.getJSONObject("picture").getJSONObject("data").getString("url"));
+                                        } catch (JSONException e){
+                                            e.printStackTrace();
+
+                                        }
+
+                                        editor.commit();
+                                    }
+                                });
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,email,picture");
+                        request.setParameters(parameters);
+                        request.executeAsync();
                     }
 
                     @Override
@@ -96,6 +136,12 @@ public class SignInActivity extends AppCompatActivity {
                         // App code
                     }
                 });
+
+        if (AccessToken.getCurrentAccessToken() != null){
+
+            Log.d("USER", sharedPref.getAll().toString());
+            buttonLogin.setText("Continue as " + sharedPref.getString("email", ""));
+        }
     }
 
 
